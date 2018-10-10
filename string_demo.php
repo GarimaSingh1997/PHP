e16b19ee8073bbbae2a196784a8b8a745a3a6bbe
GarimaSingh1997 <42217662+GarimaSingh1997@users.noreply.github.com>
2018-10-10 23:43:05 +0530

    Add files via upload

---
+++ string_demo.php
@@ -0,0 +1,9 @@
+<?php
+$firstName = "Garima";
+$lastName = "Singh";
+print("Hello, my name is $firstName<br>");
+print("Length of my first name is ".strlen($firstName)."<br>");
+$fullName = $firstName." ".$lastName;
+print("My full name is: ".$fullName."<br>");
+print("Position of First Name is: ".strpos($fullName, $firstName)."<br>");
+print("Position of last name is :".strpos($fullName, $lastName));
